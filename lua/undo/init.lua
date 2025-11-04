798e5a1bd99f66b341834b3203883cafa38db163
Brayan Ocampo <mrx.source.mrx@gmail.com>
2025-11-03 19:58:51 -0500

    config for undo file

--- lua/undo/init.lua
+++ lua/undo/init.lua
@@ -1,7 +1,9 @@
--- Crear un directorio para los archivos de undo si no existe
-vim.o.undodir = vim.fn.stdpath('config') .. '/undo'
-vim.o.undofile = true  -- Habilitar la persistencia de deshacer
+vim.o.backup = true
+vim.o.backupdir = vim.fn.stdpath('cache') .. '/nvim/backup'
+vim.o.swapfile = true
+vim.o.directory = vim.fn.stdpath('cache') .. '/nvim/swap'
 
--- Opciones adicionales
-vim.o.undolevels = 1000  -- Número de niveles de deshacer
-vim.o.undoreload = 10000 -- Cuántos cambios almacenar cuando cargas un archivo
+vim.o.undofile = true
+vim.o.undodir = vim.fn.stdpath('cache') .. '/nvim/undo'
+
+vim.o.writebackup = true
